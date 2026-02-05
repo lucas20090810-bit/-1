5e0d69e015aea66d85d25f7eac5c5b322603f3bc
lucas20090810-bit <lucas20090810@gmail.com>
2026-02-05 11:45:48 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { jsxLocPlugin } from "@builder.io/vite-plugin-jsx-loc";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import fs from "node:fs";
@@ -7,7 +6,7 @@ import { defineConfig } from "vite";
 
 
 
-const plugins = [react(), tailwindcss(), jsxLocPlugin()];
+const plugins = [react(), tailwindcss()];
 
 export default defineConfig({
   plugins,
